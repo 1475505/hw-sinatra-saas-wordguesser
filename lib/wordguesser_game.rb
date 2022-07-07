9c54a31fe6e7b3392d39bfdde40165fb7d04dfa0
1475505 <liul2566@bupt.edu.cn>
2022-07-08 00:33:37 +0800

    finish part 1

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -1,5 +1,9 @@
 class WordGuesserGame
 
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  attr_accessor :word_with_guesses
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/wordguesser_game_spec.rb pass.
 
@@ -7,6 +11,9 @@ class WordGuesserGame
 
   def initialize(word)
     @word = word
+    @word_with_guesses = '-' * word.length
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
   # You can test it by installing irb via $ gem install irb
@@ -20,6 +27,43 @@ class WordGuesserGame
     Net::HTTP.new('randomword.saasbook.info').start { |http|
       return http.post(uri, "").body
     }
+  end
+
+  def guess(letter)
+    if letter == nil || (letter =~ /^\w+$/i) == nil
+      raise ArgumentError
+    end
+
+    letter = letter.downcase
+
+    if @guesses.include?(letter) || @wrong_guesses.include?(letter)
+      return false
+    end
+
+    if @word.include?(letter)
+      @guesses += letter
+      while (i = @word.index(letter)) != nil
+        @word_with_guesses[i] = letter
+        @word[i] = '-'
+      end
+      true
+    else
+      @wrong_guesses += letter
+      false
+    end
+  end
+
+  def check_win_or_lose
+    # code here
+    if @wrong_guesses.length >= 7
+      return :lose
+    end
+
+    if @word_with_guesses.include?('-')
+      :play
+    else
+      :win
+    end
   end
 
 end
---
+++ notes.md
@@ -0,0 +1,5 @@
+---
+ref: 
+https://en.wikibooks.org/wiki/Ruby_Programming
+
+https://rubyapi.org/
